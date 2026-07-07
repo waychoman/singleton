676b7968ef2f90d9be0a538d353cf8fb588c5110
4D-25\koyo <a>
2026-07-07 14:45:35 +0900

    起動するとタイトル画面が表示されるようにしました。 何かキーを押すとゲーム画面に遷移するようにしました。 ゲーム画面で何かしらの処理をすると、ゲームオーバーもしくはゲームクリア画面を表示して、その後タイトル画面に戻るようにしました。

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -1,20 +1,93 @@
-﻿// ConsoleApplication1.cpp : このファイルには 'main' 関数が含まれています。プログラム実行の開始と終了がそこで行われます。
-//
+﻿#include <iostream>
+#include <Windows.h>
 
-#include <iostream>
+class GameManager {
+private:
+	GameManager() {}
 
-int main()
-{
-    std::cout << "Hello World!\n";
+public:
+
+	//コピー代入の禁止
+	GameManager(const GameManager&) = delete;
+
+	GameManager& operator=(const GameManager&) = delete;
+
+	static GameManager& Instance() {
+		static GameManager instance; //c++11以降のスレッドセーフなローカルstatic
+		return instance;
+	}
+
+	void GameTitle() {
+		// ゲームタイトル表示処理
+		std::cout << "=== タイトル ===\n";
+		std::cout << "Space: スタート\n";
+	}
+
+	void GameGamen() {
+		// ゲーム画面表示処理
+		std::cout << "=== ゲーム画面 ===\n";
+		std::cout << "Z: クリア, X: ゲームオーバー\n";
+	}
+	void GameOver() {
+		// ゲームオーバー表示処理
+		std::cout << "=== ゲームオーバー ===\n";
+		std::cout << "Space: タイトルに戻る\n";
+	}
+	void GameClear() {
+		// ゲームクリア表示処理
+		std::cout << "=== ゲームクリア ===\n";
+		std::cout << "Space: タイトルに戻る\n";
+	}
+};
+
+// 使用例
+int main() {
+	// シーン: 0=Title, 1=Play, 2=GameOver, 3=GameClear
+	int scene = 0;
+	bool running = true;
+
+	// 起動時にタイトル表示
+	GameManager::Instance().GameTitle();
+
+	while (running) {
+		// タイトル -> スタート
+		if (scene == 0) {
+			if (GetAsyncKeyState(VK_SPACE) & 0x8000) {
+				GameManager::Instance().GameGamen();
+				scene = 1;
+				
+			}
+		}
+		// プレイ中の入力（Z: クリア, X: オーバー）
+		else if (scene == 1) {
+			if (GetAsyncKeyState('Z') & 0x8000) {
+				GameManager::Instance().GameClear();
+				scene = 3;
+				
+			}
+			else if (GetAsyncKeyState('X') & 0x8000) {
+				GameManager::Instance().GameOver();
+				scene = 2;
+				
+			}
+		}
+		// 結果画面 -> タイトルへ
+		else if (scene == 2 || scene == 3) {
+			if (GetAsyncKeyState(VK_SPACE) & 0x8000) {
+				GameManager::Instance().GameTitle();
+				scene = 0;
+				
+			}
+		}
+
+		// 終了（Esc）
+		if (GetAsyncKeyState(VK_ESCAPE) & 0x8000) {
+			running = false;
+			
+		}
+
+		
+	}
+
+	return 0;
 }
-
-// プログラムの実行: Ctrl + F5 または [デバッグ] > [デバッグなしで開始] メニュー
-// プログラムのデバッグ: F5 または [デバッグ] > [デバッグの開始] メニュー
-
-// 作業を開始するためのヒント: 
-//    1. ソリューション エクスプローラー ウィンドウを使用してファイルを追加/管理します 
-//   2. チーム エクスプローラー ウィンドウを使用してソース管理に接続します
-//   3. 出力ウィンドウを使用して、ビルド出力とその他のメッセージを表示します
-//   4. エラー一覧ウィンドウを使用してエラーを表示します
-//   5. [プロジェクト] > [新しい項目の追加] と移動して新しいコード ファイルを作成するか、[プロジェクト] > [既存の項目の追加] と移動して既存のコード ファイルをプロジェクトに追加します
-//   6. 後ほどこのプロジェクトを再び開く場合、[ファイル] > [開く] > [プロジェクト] と移動して .sln ファイルを選択します
