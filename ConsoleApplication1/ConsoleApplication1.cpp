1e5e5356074347a13cdcfd10be7f541a2b8d5299
4D-25\koyo <a>
2026-07-07 15:39:29 +0900

    1/8192のゲーム風にしました

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -1,5 +1,7 @@
 ﻿#include <iostream>
 #include <Windows.h>
+#include <cstdlib>
+#include <ctime>
 
 class GameManager {
 private:
@@ -26,7 +28,9 @@ public:
 	void GameGamen() {
 		// ゲーム画面表示処理
 		std::cout << "=== ゲーム画面 ===\n";
-		std::cout << "Z: クリア, X: ゲームオーバー\n";
+		std::cout << "A または D を選択してください（当たり 50% / ハズレ 50%）\n";
+		std::cout << "目的: 当たりを13回連続で出すとクリア (1/8192)\n";
+		std::cout << "A = 左, D = 右\n";
 	}
 	void GameOver() {
 		// ゲームオーバー表示処理
@@ -42,9 +46,13 @@ public:
 
 // 使用例
 int main() {
+	// 乱数初期化
+	std::srand(static_cast<unsigned>(std::time(nullptr)));
+
 	// シーン: 0=Title, 1=Play, 2=GameOver, 3=GameClear
 	int scene = 0;
 	bool running = true;
+	int consecutiveHits = 0; // 連続当たり回数
 
 	// 起動時にタイトル表示
 	GameManager::Instance().GameTitle();
@@ -53,22 +61,35 @@ int main() {
 		// タイトル -> スタート
 		if (scene == 0) {
 			if (GetAsyncKeyState(VK_SPACE) & 0x8000) {
+				consecutiveHits = 0;
 				GameManager::Instance().GameGamen();
 				scene = 1;
-				
+				Sleep(150); // 簡易デバウンス
 			}
 		}
-		// プレイ中の入力（Z: クリア, X: オーバー）
+		// プレイ中: A または D で選択（ランダムで正解側を決める、プレイヤーが当てれば当たり）
 		else if (scene == 1) {
-			if (GetAsyncKeyState('Z') & 0x8000) {
-				GameManager::Instance().GameClear();
-				scene = 3;
-				
-			}
-			else if (GetAsyncKeyState('X') & 0x8000) {
-				GameManager::Instance().GameOver();
-				scene = 2;
-				
+			if (GetAsyncKeyState('A') & 0x8000 || GetAsyncKeyState('D') & 0x8000) {
+				int player = (GetAsyncKeyState('A') & 0x8000) ? 0 : 1; // A=0, D=1
+				int correct = std::rand() % 2; // 0 or 1 が正解側
+				if (player == correct) {
+					++consecutiveHits;
+					// 確率表示: 1 / (2^consecutiveHits)
+					int denom = 1 << consecutiveHits; // 2^n（consecutiveHits <= 13 なので安全）
+					std::cout << "当たり! 連続 " << consecutiveHits << " 回 (確率: 1/" << denom << ")\n";
+					if (consecutiveHits >= 13) {
+						GameManager::Instance().GameClear();
+						scene = 3;
+					}
+				}
+				else {
+					// ハズレ -> ゲームオーバー
+					std::cout << "ハズレ...\n";
+					consecutiveHits = 0;
+					GameManager::Instance().GameOver();
+					scene = 2;
+				}
+				Sleep(150); // デバウンス
 			}
 		}
 		// 結果画面 -> タイトルへ
@@ -76,17 +97,18 @@ int main() {
 			if (GetAsyncKeyState(VK_SPACE) & 0x8000) {
 				GameManager::Instance().GameTitle();
 				scene = 0;
-				
+				Sleep(150);
 			}
 		}
 
 		// 終了（Esc）
 		if (GetAsyncKeyState(VK_ESCAPE) & 0x8000) {
 			running = false;
-			
+			Sleep(150);
 		}
 
-		
+		// 軽い待ち（CPU負荷低減）
+		Sleep(10);
 	}
 
 	return 0;
